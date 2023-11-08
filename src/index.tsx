c0a0e982cf0fb92fd3bb396a9546d3b377cae18f
khushnudeshtemirov <khushnudeshtemirov@gmail.com>
2023-11-08 15:54:54 +0500

    Setup redux to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import "./index.css";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <App />
-      </Suspense>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <App />
+        </Suspense>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,32 @@
+import { FormEvent } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="w-full h-screen flex items-center justify-center flex-col gap-4">
+        <input
+          type="text"
+          className="border rounded p-2 px-3 outline-none"
+          placeholder="Enter username"
+        />
+        <input
+          type="password"
+          className="border rounded p-2 px-3 outline-none"
+          placeholder="Enter  password"
+        />
+        <button
+          onClick={handleSubmit}
+          className="bg-green-500 text-white rounded p-2 px-4"
+        >
+          Login
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
